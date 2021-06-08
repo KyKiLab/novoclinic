6e65fed54271439415331c887a66fbfc3a679694
maralov <aralov.mykhailo@gmail.com>
2021-06-08 08:20:49 +0300

    header nav adaptive

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,13 +1,29 @@
 import $ from "jquery";
+const refs = {
+  $dropDownBtn: $('.js-dropdown-btn'),
+  $dropDownContent: $('.js-dropdown-content'),
+  burgerBtn: document.querySelector('.js-toggle-menu'),
+  header: document.querySelector('#header'),
+
+}
 
 $(function () {
-  const refs = {
-    $dropDownBtn: $('.js-dropdown-btn'),
-    $dropDownContent: $('.js-dropdown-content'),
-  }
-
   refs.$dropDownBtn.on('click', e => {
     $(e.target).toggleClass('is-active');
     refs.$dropDownContent.fadeToggle(100);
   })
 });
+
+const toggleMenu = (e) => {
+
+  refs.burgerBtn.classList.toggle('toggle-menu--on');
+  refs.header.classList.toggle('mob-menu-show');
+  document.body.classList.toggle('scrolled');
+};
+
+
+refs.burgerBtn && refs.burgerBtn.addEventListener('click', toggleMenu);
+
+
+
+
